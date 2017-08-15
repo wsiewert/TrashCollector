d826ea8d8060b6c12d2d290d67dd912bdbc2ae7f
wsiewert <wsiewert94@gmail.com>
2017-08-15 16:39:17 -0500

    added customer table

---
+++ TrashCollector/TrashCollector/Migrations/201708152132537_added customer table.cs
@@ -0,0 +1,28 @@
+namespace TrashCollector.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedcustomertable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        AmountOwed = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
